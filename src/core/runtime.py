0bd5bc8ff6894127971a0c79616ec84049cb4251
KirillYuk <qwertyunisky@gmail.com>
2026-04-17 20:26:52 +0300

    add count + sleeptime

--- src/core/runtime.py
+++ src/core/runtime.py
@@ -1,9 +1,13 @@
 import time
 
+
 def run(iterator, seconds):
     end_time = time.time() + seconds
+    count = 0
 
     for price in iterator:
         if time.time() > end_time:
             break
-        print(price)
+        count+=1
+        print(count, price)
+        time.sleep(0.1)
