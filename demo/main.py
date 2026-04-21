007bc9f035922208fe142d74f732fd994db829db
KirillYuk <qwertyunisky@gmail.com>
2026-04-21 16:05:50 +0300

    add symbol input

--- demo/main.py
+++ demo/main.py
@@ -2,6 +2,7 @@ from market.generator import price_generator
 from core.runtime import run
  
 
+symbol = input("symbol: ")
 seconds = float(input("seconds: "))
 gen = price_generator(symbol="CRPR", start_price=50000.0, volatility=10)
 run(gen, seconds)
