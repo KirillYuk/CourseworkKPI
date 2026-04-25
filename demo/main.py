aa1b9e3d32d4144bab5f54817fd588b3d40afca9
KirillYuk <qwertyunisky@gmail.com>
2026-04-25 12:20:04 +0300

    stream/info commands

--- demo/main.py
+++ demo/main.py
@@ -1,9 +1,20 @@
 from market.generator import price_generator
 from core.runtime import run
- 
+from market.coins import get_coin_info 
 
+
+command = input("command (stream/info): ")
 symbol = input("symbol: ")
-seconds = float(input("seconds: "))
-gen = price_generator(symbol=symbol, start_price=50000.0, volatility=10)
-run(gen, seconds)
- 
+
+if command == "stream":
+    seconds = float(input("seconds: "))
+    gen = price_generator(symbol=symbol, start_price=50000.0, volatility=10)
+    run(gen, seconds)
+
+elif command == "info":
+    info = get_coin_info(symbol)
+    if info:
+        for key, value in info.items():
+            print(key + ":", value)
+    else:
+        print("coin not found", symbol)
