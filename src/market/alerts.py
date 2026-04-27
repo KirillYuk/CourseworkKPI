c2d68ede5dfaf13918549aa333a07797be329508
KirillYuk <qwertyunisky@gmail.com>
2026-04-27 23:53:10 +0300

    add BiQueue from lab4

---
+++ src/market/alerts.py
@@ -0,0 +1,20 @@
+class BiQueue:
+    def __init__(self):
+        self.queue = []
+        self.counter = 0
+        
+    def enqueue(self, item, priority):
+        self.queue.append((self.counter, priority, item))
+        self.counter += 1
+
+    def dequeue(self, mode):
+        if len(self.queue) == 0:
+            return None
+        
+        if mode == 'highest':
+            item = max(self.queue, key=lambda x: x[1])
+        elif mode == 'lowest':
+            item = min(self.queue, key=lambda x: x[1])
+
+        self.queue.remove(item)
+        return item[2]
