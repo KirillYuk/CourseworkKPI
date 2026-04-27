8fb0fd1f562290ca5e5b6a16e296327f92a93e02
KirillYuk <qwertyunisky@gmail.com>
2026-04-28 00:22:53 +0300

    add alerts

--- src/core/runtime.py
+++ src/core/runtime.py
@@ -1,5 +1,6 @@
 import time
 from rich import print
+from market.alerts import BiQueue
 
 
 def run(iterator, seconds):
@@ -8,6 +9,7 @@ def run(iterator, seconds):
     total = 0
     min_price = None
     max_price = None
+    alert_queue = BiQueue()
 
 
     for tick in iterator:
@@ -24,7 +26,31 @@ def run(iterator, seconds):
 
         avg = round(total/count, 2)
 
-
+        rsi = tick["technical"]["rsi"]
+        signal = tick["technical"]["signal"]
+        
+        if signal == "BUY":
+            alert_queue.enqueue(
+                {"symbol": tick["symbol"],
+                 "rsi": rsi,
+                 "signal": signal},
+                priority=3
+            )
+        
+        elif signal == "SELL":
+            alert_queue.enqueue(
+                {"symbol": tick["symbol"],
+                 "rsi": rsi,
+                 "signal": signal},
+                priority=2
+            )
+        
         print(f"[white]{count}[/white] [red]{tick["symbol"]}[/red] [green]{round(tick["price"], 2)}[green]", "   avg: ", avg, "   min: ", min_price, "   max: ", max_price)
         
         time.sleep(0.01)
+        
+    print("\n[bold yellow]ALERTS[/bold yellow]")
+    alert = alert_queue.dequeue("highest")
+    while alert != None:
+        print(f"[red]{alert["signal"]}[/red] {alert["symbol"]} RSI: {alert["rsi"]}")
+        alert = alert_queue.dequeue("highest")
