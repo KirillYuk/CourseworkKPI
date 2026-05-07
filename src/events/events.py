32f3eb0717e1a007b53797a4b3f17cb93393e655
KirillYuk <qwertyunisky@gmail.com>
2026-05-08 00:32:44 +0300

    EventEmitter (sub, unsub, emit)

---
+++ src/events/events.py
@@ -0,0 +1,27 @@
+class EventEmitter:
+    def __init__(self):
+        self.listeners = {}
+        
+    def subscribe(self, event, callback):
+        if event not in self.listeners:
+            self.listeners[event] = []
+        self.listeners[event].append(callback)
+        
+    def unsubscribe(self, event, callback):
+        if event in self.listeners:
+            self.listeners[event].remove(callback)
+            
+    def emit(self, event, data):
+        if event in self.listeners:
+            for callback in self.listeners:
+                callback(data)
+                
+                
+def on_buy_signal(data):
+    print(f"[event] BUY signal {data["symbol"]} RSI: {data["rsa"]}")
+        
+def on_sell_signal(data):
+        print(f"[event] SELL signal {data["symbol"]} RSI: {data["rsa"]}")
+        
+def on_prise_threshold(data):
+    print(f"[event] PRICE signal {data["symbol"]} price: {data["price"]}")
