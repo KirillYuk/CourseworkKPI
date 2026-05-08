6fc0cb950b9f5be51f39f7104d1042e67dbc0220
KirillYuk <qwertyunisky@gmail.com>
2026-05-08 13:33:56 +0300

    add AuthProxy

---
+++ src/core/proxy.py
@@ -0,0 +1,21 @@
+class AuthProxy:
+    def __init__(self, method="api_key", key=None, token=None):
+        self.method = method
+        self.key = key
+        self.token = token
+        
+    def get_headers(self):
+        if self.method == "api_key":
+            return {"x-api-key": self.key}
+        elif self.method == "jwt":
+            return {"Authorization": "Bearer " + self.token}
+        elif self.method == "oauth":
+            return {"Authorization": "OAuth " + self.token}
+        return {}
+    
+    def get(self, url):
+        import requests
+        headers = self.get_headers()
+        response = requests.get(url, headers=headers)
+        return response.json()
+    
